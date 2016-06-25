a9b6db379da5a2e0770b846704530cf5e9188183
Peter Hong <psehong@gmail.com>
2016-06-25 10:05:28 -0400

    Kill entire process on interrupt

--- obs/__main__.py
+++ obs/__main__.py
@@ -1,6 +1,8 @@
 """ Entry point for obs """
 
+import os
 import sys
+import signal
 import getopt
 import logging
 import concurrent.futures as futures
@@ -18,18 +20,19 @@ def main(argv):
         format='[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
     )
 
+    tpe = futures.ThreadPoolExecutor(max_workers=24)
     try:
         _, args = getopt.getopt(argv, "c", ["config"])
 
         LOG.info("Loading config.yml from: %s", args[0])
         relays = ObsConfig.parse_obs_config(args[0])
-
-        tpe = futures.ThreadPoolExecutor(max_workers=24)
         relay_futures = tpe.map(lambda relay: relay.run(), relays)
         futures.wait(list(relay_futures))
     except getopt.GetoptError as error:
         LOG.error("Must supply config YAML path as first argument: %s", error)
-        sys.exit(2)
+    finally:
+        tpe.shutdown(False)
+        os.kill(os.getpid(), signal.SIGUSR1)
 
 
 if __name__ == "__main__":
