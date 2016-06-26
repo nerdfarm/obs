41c1010d992a5d27a935d2b6df7664355a5b8e4f
Peter Hong <psehong@gmail.com>
2016-06-26 17:10:44 -0400

    rename ete for clarity

--- test/integration_test/test_ete_hangouts_to_mosquitto.py
+++ test/integration_test/test_ete_hangouts_to_mosquitto.py
@@ -7,13 +7,13 @@ from nose.plugins.attrib import attr
 
 
 @attr(speed='slow')
-class TestEteMosquittoHangouts(TestCase):
+class TestEteHangoutsToMosquitto(TestCase):
 
-    def test_ete_mosquitto_hangouts(self):
+    def test_ete_hangouts_to_mosquitto(self):
         ete_sp = subprocess.Popen([
             "python3",
             "-m",
-            "test.integration_test.ete_mosquitto_hangouts",
+            "test.integration_test.ete_hangouts_to_mosquitto",
             '--auth_token_path=' + os.path.expanduser('~/' + os.environ['TEST_HANGOUTS_AUTH_TOKEN_NAME']),
             '--conversation_id=' + os.environ['HANGOUTS_CONVERSATION_ID'],
             '--config=' + os.path.expanduser('~/' + os.environ['TEST_CONFIG_NAME'])
